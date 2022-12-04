e338e27ae78be1274835e537966afe4020d655c1
cattynip <cattynip.cattynip@gmail.com>
2022-12-04 12:42:50 -0800

    feat(image): change images

--- components/config.tsx
+++ components/config.tsx
@@ -29,32 +29,8 @@ export default function Header({ title = titleDefault }) {
         name="viewport"
         content="width=device-width, minimum-scale=1, initial-scale=1.0"
       />
-      <meta name="theme-color" content="#000" />
-
-      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-      <link
-        rel="apple-touch-icon"
-        sizes="16x16"
-        href="/icons/favicon-16x16.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="32x32"
-        href="/icons/favicon-32x32.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/icons/apple-touch-icon.png"
-      />
-      <link rel="manifest" href="/manifest.json" />
-      <link
-        rel="mask-icon"
-        color="#000000"
-        href="/icons/safari-pinned-tab.svg"
-      />
-      <link rel="apple-touch-startup-image" href="/startup.png" />
       <link rel="shortcut icon" href="/icons/favicon.ico" />
+      <link rel="icon" type="image/x-icon" href="/icons/favicon.ico" />
     </Head>
   );
 }
