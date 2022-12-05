34f6afd0ef0b7b158d2ed924d41e65a1dedfa590
cattynip <cattynip.cattynip@gmail.com>
2022-12-04 20:57:17 -0800

    feat(component): develop atom component

---
+++ components/canvas/Atom.tsx
@@ -0,0 +1,25 @@
+import { useRecoilValue } from 'recoil';
+import { atomState, atomTypeState, extraState } from '@components/states';
+import { useRef } from 'react';
+import DaltonAtomModel from './Dalton';
+import ThomsonAtomModel from './Thomson';
+import BohrAtomModel from './Bohr';
+
+const Atom = () => {
+  const atomInfor = useRecoilValue(atomState);
+  const atomType = useRecoilValue(atomTypeState);
+
+  return (
+    <>
+      {atomType === 'Dalton' ? <DaltonAtomModel /> : null}
+      {atomType === 'Thomson' ? (
+        <ThomsonAtomModel atomicNumber={atomInfor.atomicNumber} />
+      ) : null}
+      {atomType === 'Bohr' ? (
+        <BohrAtomModel atomicNumber={atomInfor.atomicNumber} />
+      ) : null}
+    </>
+  );
+};
+
+export default Atom;
