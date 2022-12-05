408747325e9acc3064a40b3c979f4d57fcd71312
cattynip <cattynip.cattynip@gmail.com>
2022-12-04 16:04:04 -0800

    feat(component): helper controller

--- components/canvas/Scene.tsx
+++ components/canvas/Scene.tsx
@@ -1,23 +1,27 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload } from '@react-three/drei';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { extraState } from '@components/states';
 
 interface IScene {
   children: React.ReactNode;
 }
 
 const Scene = ({ children, ...props }: IScene & any) => {
+  const helpers = useRecoilValue(extraState);
+
   return (
     <Canvas {...props}>
       <directionalLight position={[10, 10, 10]} intensity={0.75} />
       <directionalLight position={[-10, -10, -10]} intensity={0.75} />
       <ambientLight intensity={0.13} />
       {children}
-      <gridHelper scale={6} />
-      <axesHelper scale={[80, 80, 80]} />
-      <polarGridHelper scale={3} />
+      {helpers.gridHelper ? <gridHelper scale={8} /> : null}
+      {helpers.axesHelper ? <axesHelper scale={40} /> : null}
+      {helpers.polarGridHelper ? <polarGridHelper scale={5} /> : null}
+      {helpers.cameraController ? <OrbitControls /> : null}
       <Preload all />
-      <OrbitControls />
     </Canvas>
   );
 };
---
+++ components/dom/HelperController.tsx
@@ -0,0 +1,52 @@
+import { extraState } from '@components/states';
+import { ChangeEvent } from 'react';
+import { useRecoilState } from 'recoil';
+import BasicInformationBox from './BasicInformationBox';
+
+const HelperController = () => {
+  const [helperState, setHelperState] = useRecoilState(extraState);
+
+  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const checked = event.currentTarget.checked;
+    const currentValue = event.currentTarget.value;
+
+    setHelperState(prev => {
+      const returnObject = { ...prev };
+
+      Object.keys(returnObject).map((key, _idx) => {
+        if (key === currentValue) {
+          returnObject[key] = checked;
+        }
+      });
+
+      return returnObject;
+    });
+  };
+
+  return (
+    <BasicInformationBox
+      direction={{
+        topBottom: 'top',
+        leftRight: 'left'
+      }}
+    >
+      <ul>
+        {Object.keys(helperState).map((key, idx) => (
+          <li key={idx}>
+            <input
+              className="mr-2 marker:bg-teal-300"
+              type="checkbox"
+              id={idx.toString()}
+              defaultChecked={helperState[key]}
+              onChange={onInputChange}
+              value={key}
+            />
+            <label htmlFor={idx.toString()}>{key}</label>
+          </li>
+        ))}
+      </ul>
+    </BasicInformationBox>
+  );
+};
+
+export default HelperController;
