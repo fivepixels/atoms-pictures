6661171d4592d2f37d79fb755142d8b28c2738ba
cattynip <cattynip.cattynip@gmail.com>
2022-12-04 16:03:43 -0800

    feat(component): atom information box

---
+++ components/dom/AtomInformationBox.tsx
@@ -0,0 +1,38 @@
+import { findAtomBy, IAtomData } from '@components/data/data';
+import { atomState } from '@components/states';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import BasicInformationBox from './BasicInformationBox';
+
+const AtomInformationBox = () => {
+  const [currentAtom, _setCurrentAtom] = useRecoilState(atomState);
+  const [atom, setAtom] = useState<IAtomData>(
+    findAtomBy({
+      by: 'symbol',
+      content: 'H'
+    })[0]
+  );
+
+  useEffect(() => {
+    setAtom(currentAtom);
+  }, [atom, currentAtom]);
+
+  return (
+    <BasicInformationBox
+      direction={{
+        topBottom: 'bottom',
+        leftRight: 'left'
+      }}
+    >
+      <h1 className="text-7xl">{atom.symbol}</h1>
+      <ul className="text-xs">
+        <li>Symbol : {atom.symbol}</li>
+        <li>Atomic Number : {atom.atomicNumber}</li>
+        <li>Mass : {atom.atomicMass}</li>
+        <li>Classification : {atom.classification}</li>
+      </ul>
+    </BasicInformationBox>
+  );
+};
+
+export default AtomInformationBox;
