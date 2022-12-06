20e7a89cc91df32fb492613f6541ec6e1bc3e1a4
cattynip <cattynip.cattynip@gmail.com>
2022-12-06 01:20:04 -0800

    refactor(component): fix recoil error on useKeyboard

--- components/dom/useKeyboard.tsx
+++ components/dom/useKeyboard.tsx
@@ -5,15 +5,20 @@ interface IUseKeyboard {
   fn: () => void;
 }
 
-const useKeyboard = (infor: IUseKeyboard[], log: boolean) => {
+const useKeyboard = (infor: IUseKeyboard[]) => {
   const keydown = (event: KeyboardEvent) => {
-    if (log) console.log(`KEY PRESSED : ${event.key}`);
     infor.map(value => {
       if (typeof value.key === 'string' && event.key === value.key) {
+        value.fn();
+        value.fn();
+        value.fn();
         value.fn();
       } else if (typeof value.key === 'object') {
         value.key.map(keyValue => {
           if (keyValue === event.key) {
+            value.fn();
+            value.fn();
+            value.fn();
             value.fn();
           }
         });
